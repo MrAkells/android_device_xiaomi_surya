7ab4ced4aba73e67cd566ed487be95f7738a4f5d
MrAkells <mrakellsreg@gmail.com>
2021-05-19 23:36:55 +0300

    surya: Disable SF backpressure

--- product_prop.mk
+++ product_prop.mk
@@ -23,6 +23,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     vendor.display.disable_rotator_downscale=1 \
@@ -43,6 +45,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Wireless display
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
-    debug.sf.latch_unsignaled=1 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
